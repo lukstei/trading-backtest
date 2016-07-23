e2d9310952b508668ea321a72c09c268851545bf
Lukas Steinbrecher <steinbrecherluk@gmail.com>
2016-07-23 16:05:06 +0200

    doc

--- src/main/r/report.r
+++ src/main/r/report.r
@@ -1,5 +1,5 @@
-orders <- read.csv('/var/folders/_5/jv4ptlps2ydb4_ptyj_l2y100000gn/T/out-7373128809679149089.csv')
-statistics <- read.csv('/var/folders/_5/jv4ptlps2ydb4_ptyj_l2y100000gn/T/out-1984107031930922019.csv')
+orders <- read.csv('/var/folders/_5/jv4ptlps2ydb4_ptyj_l2y100000gn/T/out-7373128809679149089.csv') # insert output file here
+statistics <- read.csv('/var/folders/_5/jv4ptlps2ydb4_ptyj_l2y100000gn/T/out-1984107031930922019.csv') # insert output file here
 statistics$date = as.Date(statistics$date)
 
 deposit = 15000
@@ -8,9 +8,9 @@ plot(statistics$date, (deposit+statistics$pl)/deposit-1, type='l', xlab = 'Date'
 pctWin = sum(orders$pl > 0)/length(orders$pl)
 print(paste('Win percentage: ', pctWin*100, '%'))
 
-## Cointegration strategy specific
+## specific to the cointegration strategy
 
-kalmanStatistics = read.csv('/var/folders/_5/jv4ptlps2ydb4_ptyj_l2y100000gn/T/out-3935155810073028635.csv')
+kalmanStatistics = read.csv('/var/folders/_5/jv4ptlps2ydb4_ptyj_l2y100000gn/T/out-3935155810073028635.csv') # insert output file here
 kalmanStatistics$date = as.Date(kalmanStatistics$date)
 
 plot(kalmanStatistics$beta, type='l')
