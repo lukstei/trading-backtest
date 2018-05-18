a0e2564d9b620bf4750f501bdec6edc064daf349
Andrew Kreimer <algonell@gmail.com>
2018-05-18 19:23:20 +0300

    fixed historical data retrieval via yahoofinance-api

--- src/main/java/org/lst/trading/lib/util/HistoricalPriceService.java
+++ src/main/java/org/lst/trading/lib/util/HistoricalPriceService.java
@@ -1,8 +1,7 @@
 package org.lst.trading.lib.util;
 
 import org.lst.trading.lib.series.DoubleSeries;
-import rx.Observable;
 
 public interface HistoricalPriceService {
-    Observable<DoubleSeries> getHistoricalAdjustedPrices(String symbol);
+    DoubleSeries getHistoricalAdjustedPrices(String symbol);
 }
